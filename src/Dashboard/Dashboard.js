90acbf6aaa55b557b3511f86e30f779f4cb1ad63
Hridoyshom <hshom827@gmail.com>
2022-04-05 01:15:38 +0600

    chart added

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import { Bar, BarChart, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
+        <div className='  flex gap-4 '>
+            <div >
+                <LineChart width={400} height={400} data={data}>
+                    <Line dataKey={'sell'}  ></Line>
+                    <XAxis dataKey={'month'} ></XAxis>
+                    <YAxis dataKey={'sell'} />
+                    <Tooltip />
+
+                </LineChart>
+            </div>
+            <div className=' ml-20 ' >
+                <BarChart width={400} height={400} data={data} >
+                    <Bar dataKey={'sell'} fill="#8884d8" ></Bar>
+                    <XAxis dataKey={'month'} ></XAxis>
+                    <YAxis dataKey={'sell'} />
+                    <Tooltip />
+                </BarChart>
+            </div>
 
         </div>
+
     );
 };
 
--- src/ReviewCard/ReviewCard.js
+++ src/ReviewCard/ReviewCard.js
@@ -3,7 +3,7 @@ import Reviews from './../Reviews/Reviews';
 
 const ReviewCard = ({ review }) => {
     return (
-        <div className='shadow-lg rounded-2xl w-[250px] bg-white p-4  ' >
+        <div className='shadow-lg rounded-2xl w-[250px]  bg-slate-200  p-4  mt-5  ' >
             <p><span className='font-bold' >Name</span>:{review.name}</p>
             <p><span className='font-bold' >Review</span>:{review.Review}</p>
             <p><span className='font-bold' >Rating</span>:{review.Rating}</p>
